3e1302a315c7053675f34f102874b6d90ef253d2
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-25 08:32:09 +0200

    made use of a handler to render a page template component preprocessed with data for the template

--- routes/blog/index.tsx
+++ routes/blog/index.tsx
@@ -1,4 +1,52 @@
-function Blog() {
+import { Handlers, PageProps } from "$fresh/server.ts";
+
+const CategoryData = [
+  {
+    name: 'Category1',
+    status: 1,
+    date: Date.now()
+  },
+  {
+    name: 'Category2',
+    status: 1,
+    date: Date.now()
+  },
+  {
+    name: 'Category3',
+    status: 1,
+    date: Date.now()
+  },
+  {
+    name: 'Category4',
+    status: 1,
+    date: Date.now()
+  },
+  {
+    name: 'Category5',
+    status: 1,
+    date: Date.now()
+  },
+  {
+    name: 'Category6',
+    status: 1,
+    date: Date.now()
+  },
+];
+
+export const handler: Handlers = {
+  async GET(_req, ctx: any) {
+    try {
+      const categories = CategoryData;
+      return await ctx.render({ categories });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+function Blog(props: PageProps) {
+  console.log(props.data);
+  
   return (
     <div>This is the Blog Page.</div>
   );
